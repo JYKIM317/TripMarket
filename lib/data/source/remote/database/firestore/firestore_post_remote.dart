83e3501ade685ac279158c5ca74020316eb16e11
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-04-09 02:36:31 +0900

    Firestore post remote create new class

--- lib/data/source/remote/database/firestore/firestore_post_remote.dart
+++ lib/data/source/remote/database/firestore/firestore_post_remote.dart
@@ -1,1 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:trip_market/analytics.dart';
 
+class FirestorePostDocumentRemote {}
+
+class FirestorePostCollectionRemote {}
